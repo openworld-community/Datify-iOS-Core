0f0317e76e23ce9c892b30582fa5794282f08001
Alexander <a.pride.xxi@tutanota.com>
2023-08-20 18:32:50 +0300

    Initial Commit

---
+++ Datify-iOS-Core/Datify_iOS_CoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Datify_iOS_CoreApp.swift
+//  Datify-iOS-Core
+//
+//  Created by Александр Прайд on 20.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Datify_iOS_CoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
