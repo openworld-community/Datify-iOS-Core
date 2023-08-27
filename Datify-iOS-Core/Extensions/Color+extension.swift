efae305cc5fb8f8d5619c4f612ed6de37f9272b0
Sergei Volkov <s.volkov@dssl.ru>
2023-08-27 15:01:14 +0500

    SIT-20 colors added

---
+++ Datify-iOS-Core/Extensions/Color+extension.swift
@@ -0,0 +1,31 @@
+//
+//  Color+extension.swift
+//  Datify-iOS-Core
+//
+//  Created by Sergei Volkov on 27.08.2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundPrimary: Color = Color("BackgroundPrimary")
+    static let backgroundSecondary: Color = Color("BackgroundSecondary")
+    static let backgroundStroke: Color = Color("BackgroundStroke")
+
+    static let iconsPrimary: Color = Color("IconsPrimary")
+    static let iconsSecondary: Color = Color("IconsSecondary")
+    static let iconsTertiary: Color = Color("IconsTertiary")
+
+    static let accentsBlue: Color = Color("AccentsBlue")
+    static let accentsError: Color = Color("AccentsError")
+    static let accentsGreen: Color = Color("AccentsGreen")
+    static let accentsPink: Color = Color("AccentsPink")
+    static let accentsViolet: Color = Color("AccentsViolet")
+    static let accentsYellow: Color = Color("AccentsYellow")
+
+    static let textInverted: Color = Color("TextInverted")
+    static let textPrimary: Color = Color("TextPrimary")
+    static let textSecondary: Color = Color("TextSecondary")
+    static let textTertiary: Color = Color("TextTertiary")
+    static let textPrimaryLink: Color = Color("TextPrimaryLink")
+}
