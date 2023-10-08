794a97c96e72ddd477c91dbbd0ce23a455e04186
moraWoo <pkstratech@yandex.ru>
2023-10-08 14:50:42 +0600

    IOS-29_Initial_commit

---
+++ Datify-iOS-Core/Views/Main/Tabbar/ChatView.swift
@@ -0,0 +1,8 @@
+//
+//  ChatView.swift
+//  Datify-iOS-Core
+//
+//  Created by Ildar Khabibullin on 07.10.2023.
+//
+
+import Foundation
---
+++ Datify-iOS-Core/Views/Main/Tabbar/TabbarView.swift
@@ -0,0 +1,40 @@
+//
+//  TabbarView.swift
+//  Datify-iOS-Core
+//
+//  Created by Ildar Khabibullin on 07.10.2023.
+//
+
+//import SwiftUI
+//
+//enum Tab {
+//    case dating
+//    case chat
+//    case menu
+//}
+//
+//struct TabbarView: View {
+//    @State private var selectedTab: Tab = .dating
+//
+//    var body: some View {
+//        TabView(selection: $selectedTab) {
+//            TempView()
+//                .tabItem {
+//                    Text("Dating")
+//                }
+//                .tag(Tab.dating)
+//
+//            TempView()
+//                .tabItem {
+//                    Text("Chat")
+//                }
+//                .tag(Tab.chat)
+//
+//            TempView()
+//                .tabItem {
+//                    Text("Menu")
+//                }
+//                .tag(Tab.menu)
+//        }
+//    }
+//}
