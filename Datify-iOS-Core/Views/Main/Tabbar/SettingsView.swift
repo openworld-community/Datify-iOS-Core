76980d9bb4912ffc70d4d0b0ab99c86837fc8946
aleksey <a.baranov89@mail.ru>
2023-12-26 15:24:24 +0500

    #41 init commit

---
+++ Datify-iOS-Core/Views/Main/Tabbar/SettingsView.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsView.swift
+//  Datify-iOS-Core
+//
+//  Created by Алексей Баранов on 26.12.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Setting")
+    }
+}
